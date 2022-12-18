4dee2fbc679fc353816c33c3ef9eb44b2d080352
Pavel2202 <paveltimenov3@gmail.com>
2022-12-18 10:16:36 +0200

    Add Factory Design Pattern

---
+++ DesignPatterns/Factory/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Person
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}";
+    }
+}
+
+public class PersonFactory
+{
+    private int id = 0;
+
+    public Person CreatePerson(string name)
+    {
+        return new Person { Id = id++, Name = name };
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        var pf = new PersonFactory();
+        var john = pf.CreatePerson("John");
+        var jane = pf.CreatePerson("Jane");
+
+        Console.WriteLine(john);
+        Console.WriteLine(jane);
+    }
+}
