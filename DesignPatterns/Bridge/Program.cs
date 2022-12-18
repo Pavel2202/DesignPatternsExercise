58b7cfc315dec35994ea406100b4b9946ad81fb2
Pavel2202 <paveltimenov3@gmail.com>
2022-12-18 11:46:01 +0200

    Add Bridge Design Pattern

---
+++ DesignPatterns/Bridge/Program.cs
@@ -0,0 +1,60 @@
+﻿public interface IRenderer
+{
+    string WhatToRenderAs { get; }
+}
+
+public abstract class Shape
+{
+    private IRenderer renderer;
+
+    public Shape(IRenderer renderer)
+    {
+        this.renderer = renderer;
+    }
+
+    public string Name { get; set; }
+
+    public override string ToString()
+    {
+        return $"Drawing {Name} as {renderer.WhatToRenderAs}";
+    }
+}
+
+public class Triangle : Shape
+{
+    public Triangle(IRenderer renderer)
+        : base(renderer)
+    {
+        Name = "Triangle";
+    }
+}
+
+public class Square : Shape
+{
+    public Square(IRenderer renderer)
+        : base(renderer)
+    {
+        Name = "Square";
+    }
+}
+
+public class RasterRender : IRenderer
+{
+    public string WhatToRenderAs => "pixels";
+}
+
+public class VectorRender : IRenderer
+{
+    public string WhatToRenderAs => "lines";
+}
+
+public class Program
+{
+    static void Main()
+    {
+        var vectorSquare = new Square(new VectorRender());
+        var rasterTriangle = new Triangle(new RasterRender());
+        Console.WriteLine(vectorSquare.ToString());
+        Console.WriteLine(rasterTriangle.ToString());
+    }
+}
