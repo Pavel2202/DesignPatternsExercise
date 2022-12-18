211a9d498dd0294f9467fd7a78a0bb7d395830ba
Pavel2202 <paveltimenov3@gmail.com>
2022-12-18 10:38:44 +0200

    Add Adapter Design Pattern

---
+++ DesignPatterns/Adapter/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Square
+{
+    public int Side { get; set; }
+}
+
+public interface IRectangle
+{
+    int Width { get; }
+
+    int Height { get; }
+}
+
+public static class ExtensionMethods
+{
+    public static int Area(this IRectangle rc)
+    {
+        return rc.Width * rc.Height;
+    } 
+}
+
+public class SquareToRectangeAdapter : IRectangle
+{
+    private readonly Square square;
+
+    public SquareToRectangeAdapter(Square square)
+    {
+        this.square = square;
+    }
+
+    public int Width => square.Side;
+    public int Height => square.Side;
+}
+
+public class Program
+{
+    static void Main()
+    {
+        var sq = new Square { Side = 8 };
+        var adapter = new SquareToRectangeAdapter(sq);
+        Console.WriteLine(adapter.Area());
+    }
+}
