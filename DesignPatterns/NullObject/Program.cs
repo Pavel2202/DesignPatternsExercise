ab4013130f6f3797a15070e1b81681a5fa1b8f80
Pavel2202 <paveltimenov3@gmail.com>
2022-12-20 16:47:46 +0200

    Add NullObject Design Pattern

---
+++ DesignPatterns/NullObject/Program.cs
@@ -0,0 +1,53 @@
+﻿public interface ILog
+{
+    int RecordLimit { get; }
+
+    int RecordCount { get; set; }
+
+    void LogInfo(string message);
+}
+
+public class Account
+{
+    private ILog log;
+
+    public Account(ILog log)
+    {
+        this.log = log;
+    }
+
+    public void SomeOperation()
+    {
+        int c = log.RecordCount;
+        log.LogInfo("Performing an operation.");
+        if (c + 1 != log.RecordCount)
+        {
+            throw new Exception();
+        }
+        if (log.RecordCount >= log.RecordLimit)
+        {
+            throw new Exception();
+        }
+    }
+}
+
+public class NullLog : ILog
+{
+    public int RecordLimit { get; } = int.MaxValue;
+
+    public int RecordCount { get; set; } = int.MinValue;
+
+    public void LogInfo(string message)
+    {
+        ++RecordCount;
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        var a = new Account(new NullLog());
+        a.SomeOperation();
+    }
+}
