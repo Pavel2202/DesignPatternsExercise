54f335c390d04571a3aa02919afc56a608b40bd0
Pavel2202 <paveltimenov3@gmail.com>
2022-12-20 15:57:53 +0200

    Add Memento Design Pattern

---
+++ DesignPatterns/Memento/Program.cs
@@ -0,0 +1,49 @@
+﻿public class Token
+{
+    public int Value = 0;
+
+    public Token(int value)
+    {
+        Value = value;
+    }
+}
+
+public class Memento
+{
+    public List<Token> Tokens = new List<Token>();
+}
+
+public class TokenMachine
+{
+    public List<Token> Tokens = new List<Token>();
+
+    public Memento AddToken(int value)
+    {
+        return AddToken(new Token(value));
+    }
+
+    public Memento AddToken(Token token)
+    {
+        Tokens.Add(token);
+        var m = new Memento();
+        m.Tokens = Tokens.Select(t => new Token(t.Value)).ToList();
+        return m;
+    }
+
+    public void Revert(Memento m)
+    {
+        Tokens = m.Tokens.Select(mm => new Token(mm.Value)).ToList();
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        var tm = new TokenMachine();
+        tm.AddToken(1);
+        var m = tm.AddToken(2);
+        tm.AddToken(3);
+        tm.Revert(m);
+    }
+}
